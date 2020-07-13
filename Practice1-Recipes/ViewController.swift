543adb0b8d7ca7f2fbc4acf41c4b1122761e8f6d
Kristina <perekrist31@gmail.com>
2020-07-13 16:13:09 +0700

    Initial Commit

---
+++ Practice1-Recipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice1-Recipes
+//
+//  Created by Кристина Перегудова on 13.07.2020.
+//  Copyright © 2020 Кристина Перегудова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
