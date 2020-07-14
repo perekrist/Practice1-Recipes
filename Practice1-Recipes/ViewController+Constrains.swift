9efb8759174b50b99b56fa44bdf4c1da157c32dd
Kristina <perekrist31@gmail.com>
2020-07-14 12:15:00 +0700

    basic elements for main view
    * no storyboard preparation
    
    * navigation view with search controller and sort button

---
+++ Practice1-Recipes/ViewController+Constrains.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController+Constrains.swift
+//  Practice1-Recipes
+//
+//  Created by Кристина Перегудова on 13.07.2020.
+//  Copyright © 2020 Кристина Перегудова. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+extension ViewController {
+    
+    func setUpConstrains() {
+        
+    }
+    
+}
--- Practice1-Recipes/ViewController.swift
+++ Practice1-Recipes/ViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var searchController: UISearchController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        navigationItem.title = "Recipes"
+        navigationItem.largeTitleDisplayMode = .always
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Sort by", style: .plain, target: self, action: #selector(sortByTapped))
+        navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.2901960784, alpha: 1)
+
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.barTintColor = .white
+        searchController.searchBar.layer.borderColor = .none
+
+        navigationItem.searchController = searchController
+                
+        setUpConstrains()
     }
+    
+    @objc func sortByTapped() {
+        print("sortByTapped")
+    }
+    
+    
 
 
 }
